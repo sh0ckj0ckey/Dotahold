e7a8a044bfce10fc5cac571cb8f0f659408653f4
姚一鸣 <yaoyiming123@live.com>
2022-07-20 19:51:26 +0800

    m

---
+++ OpenDota-UWP/Helpers/SteamApiKey_GitIgnored.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenDota_UWP.Helpers
+{
+    /// <summary>
+    /// 这个类里面存放ApiKey，但是设置为gitignore
+    /// </summary>
+    public static class SteamApiKey
+    {
+        public static readonly string STEAM_API_KEY = "";
+    }
+}
