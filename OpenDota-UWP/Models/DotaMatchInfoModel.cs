61c43f56e03216ec37f618757e8d6c0e8fe36367
姚一鸣 <yaoyiming123@live.com>
2022-08-15 19:41:16 +0800

    m

--- OpenDota-UWP/Models/DotaMatchInfoModel.cs
+++ OpenDota-UWP/Models/DotaMatchInfoModel.cs
@@ -12,16 +12,16 @@ namespace OpenDota_UWP.Models
         public int dire_score { get; set; }
         public int duration { get; set; }
         public int first_blood_time { get; set; }
-        public int game_mode { get; set; }
-        public int lobby_type { get; set; }
-        public Picks_Bans[] picks_bans { get; set; }
-        public int[] radiant_gold_adv { get; set; }
+        public int? game_mode { get; set; }
+        public int? lobby_type { get; set; }
+        public List<Picks_Bans> picks_bans { get; set; }
+        public List<int> radiant_gold_adv { get; set; }
         public int radiant_score { get; set; }
         public bool radiant_win { get; set; }
-        public int[] radiant_xp_adv { get; set; }
+        public List<int> radiant_xp_adv { get; set; }
         public int? skill { get; set; }
         public int start_time { get; set; }
-        public Player[] players { get; set; }
+        public List<Player> players { get; set; }
     }
 
     public class Picks_Bans
@@ -37,26 +37,26 @@ namespace OpenDota_UWP.Models
     public class Player
     {
         public long match_id { get; set; }
-        public int player_slot { get; set; }
-        public int[] ability_upgrades_arr { get; set; }
+        public int? player_slot { get; set; }
+        public List<int> ability_upgrades_arr { get; set; }
         public int account_id { get; set; }
         public Additional_Units[] additional_units { get; set; }
-        public int assists { get; set; }
+        public int? assists { get; set; }
         public int? backpack_0 { get; set; }
         public int? backpack_1 { get; set; }
         public int? backpack_2 { get; set; }
         public int? backpack_3 { get; set; }
-        public int camps_stacked { get; set; }
-        public int creeps_stacked { get; set; }
-        public int deaths { get; set; }
-        public int denies { get; set; }
-        public int gold { get; set; }
-        public int gold_per_min { get; set; }
-        public int gold_spent { get; set; }
-        public int[] gold_t { get; set; }
-        public int hero_damage { get; set; }
-        public int hero_healing { get; set; }
-        public int hero_id { get; set; }
+        public int? camps_stacked { get; set; }
+        public int? creeps_stacked { get; set; }
+        public int? deaths { get; set; }
+        public int? denies { get; set; }
+        public int? gold { get; set; }
+        public int? gold_per_min { get; set; }
+        public int? gold_spent { get; set; }
+        public List<int> gold_t { get; set; }
+        public int? hero_damage { get; set; }
+        public int? hero_healing { get; set; }
+        public int? hero_id { get; set; }
         public int? item_0 { get; set; }
         public int? item_1 { get; set; }
         public int? item_2 { get; set; }
@@ -64,36 +64,36 @@ namespace OpenDota_UWP.Models
         public int? item_4 { get; set; }
         public int? item_5 { get; set; }
         public int? item_neutral { get; set; }
-        public int kills { get; set; }
-        public int last_hits { get; set; }
-        public int level { get; set; }
-        public int net_worth { get; set; }
-        public int obs_placed { get; set; }
-        public int party_id { get; set; }
-        public int party_size { get; set; }
-        public Permanent_Buffs[] permanent_buffs { get; set; }
-        public int pings { get; set; }
-        public Purchase_Log[] purchase_log { get; set; }
+        public int? kills { get; set; }
+        public int? last_hits { get; set; }
+        public int? level { get; set; }
+        public int? net_worth { get; set; }
+        public int? obs_placed { get; set; }
+        public int? party_id { get; set; }
+        public int? party_size { get; set; }
+        public List<Permanent_Buffs> permanent_buffs { get; set; }
+        public int? pings { get; set; }
+        public List<Purchase_Log> purchase_log { get; set; }
         public bool randomed { get; set; }
-        public int roshans_killed { get; set; }
-        public Runes_Log[] runes_log { get; set; }
-        public int tower_damage { get; set; }
-        public int towers_killed { get; set; }
-        public int xp_per_min { get; set; }
-        public int[] xp_t { get; set; }
+        public int? roshans_killed { get; set; }
+        public List<Runes_Log> runes_log { get; set; }
+        public int? tower_damage { get; set; }
+        public int? towers_killed { get; set; }
+        public int? xp_per_min { get; set; }
+        public List<int> xp_t { get; set; }
         public string personaname { get; set; }
         public string name { get; set; }
         public bool radiant_win { get; set; }
         public bool isRadiant { get; set; }
-        public int total_gold { get; set; }
-        public int total_xp { get; set; }
-        public int neutral_kills { get; set; }
-        public int tower_kills { get; set; }
-        public int courier_kills { get; set; }
-        public int observer_kills { get; set; }
-        public int sentry_kills { get; set; }
-        public int roshan_kills { get; set; }
-        public int buyback_count { get; set; }
+        public int? total_gold { get; set; }
+        public int? total_xp { get; set; }
+        public int? neutral_kills { get; set; }
+        public int? tower_kills { get; set; }
+        public int? courier_kills { get; set; }
+        public int? observer_kills { get; set; }
+        public int? sentry_kills { get; set; }
+        public int? roshan_kills { get; set; }
+        public int? buyback_count { get; set; }
         public int? rank_tier { get; set; }
         public Benchmarks benchmarks { get; set; }
     }
