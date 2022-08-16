993e30dce65eee49c5a4d87143248aef0944f5b1
姚一鸣 <yaoyiming123@live.com>
2022-08-16 13:33:53 +0800

    拉到英雄和物品列表前禁止点击比赛页面的按钮

---
+++ OpenDota-UWP/Converters/BoolReverseConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Markup;
+using Windows.UI.Xaml.Media;
+
+namespace OpenDota_UWP.Converters
+{
+    internal class BoolReverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            try
+            {
+                if (value != null)
+                {
+                    return bool.Parse(value?.ToString() ?? "True") ? false : true;
+                }
+            }
+            catch { }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
