213a257682f9ebe3c251d481baaca1603aa3ba60
姚一鸣 <yaoyiming123@live.com>
2025-04-21 23:52:18 +0800

    Organize code

--- Dotahold/Pages/SettingsPage.xaml.cs
+++ Dotahold/Pages/SettingsPage.xaml.cs
@@ -51,6 +51,11 @@ namespace Dotahold.Pages
             _viewModel = e.Parameter as MainViewModel;
         }
 
+        /// <summary>
+        /// 滚动时渐入标题分割线
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ScrollViewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
         {
             if (sender is ScrollViewer scrollViewer)
@@ -75,6 +80,32 @@ namespace Dotahold.Pages
             }
         }
 
+        /// <summary>
+        /// 前往应用商店评分
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void RateButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            await Launcher.LaunchUriAsync(new Uri($"ms-windows-store:REVIEW?PFN={Package.Current.Id.FamilyName}"));
+        }
+
+        /// <summary>
+        /// 双击 Logo 显示开发者选项
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Image_DoubleTapped(object sender, Windows.UI.Xaml.Input.DoubleTappedRoutedEventArgs e)
+        {
+            DevelopmentHeaderTextBlock.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            DevelopmentSettingsCard.Visibility = Windows.UI.Xaml.Visibility.Visible;
+        }
+
+        /// <summary>
+        /// 清理缓存图片
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void ClearCacheButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             if (sender is Button button)
@@ -98,44 +129,63 @@ namespace Dotahold.Pages
             }
         }
 
-        private async void RateButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-            await Launcher.LaunchUriAsync(new Uri($"ms-windows-store:REVIEW?PFN={Package.Current.Id.FamilyName}"));
-        }
-
-        private void Image_DoubleTapped(object sender, Windows.UI.Xaml.Input.DoubleTappedRoutedEventArgs e)
-        {
-            DevelopmentHeaderTextBlock.Visibility = Windows.UI.Xaml.Visibility.Visible;
-            DevelopmentSettingsCard.Visibility = Windows.UI.Xaml.Visibility.Visible;
-        }
-
+        /// <summary>
+        /// 前往 GitHub 提交问题
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void GitHubIssueButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             await Launcher.LaunchUriAsync(new Uri("https://github.com/sh0ckj0ckey/Dotahold/issues"));
         }
 
+        /// <summary>
+        /// 前往 Steam 个人主页
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void SteamPageButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             await Launcher.LaunchUriAsync(new Uri("https://steamcommunity.com/profiles/76561198194624815/"));
         }
 
+        /// <summary>
+        /// 前往 GitHub 项目页面
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void GitHubPageButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             await Launcher.LaunchUriAsync(new Uri("https://github.com/sh0ckj0ckey/Dotahold"));
         }
 
+        /// <summary>
+        /// 打开数据文件夹
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void DataFolerButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             var folder = await StorageFilesCourier.GetDataFolder();
             await Launcher.LaunchFolderAsync(folder);
         }
 
+        /// <summary>
+        /// 打开图片缓存文件夹
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void ImageCacheFolderButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             var folder = await ImageCourier.GetCacheFolderAsync();
             await Launcher.LaunchFolderAsync(folder);
         }
 
+        /// <summary>
+        /// 下次启动强制更新常量数据
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ForceUpdateButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             ConstantsCourier.ResetConstantsGottenDate();
