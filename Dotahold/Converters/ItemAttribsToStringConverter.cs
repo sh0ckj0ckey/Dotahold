7a10ae72862c8223d56b809c26fead31e1257ced
姚一鸣 <yaoyiming123@live.com>
2025-04-28 23:43:33 +0800

    Item info page

---
+++ Dotahold/Converters/ItemAttribsToStringConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dotahold.Data.DataShop;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml;
+using Dotahold.Data.Models;
+
+namespace Dotahold.Converters
+{
+    internal partial class ItemAttribsToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            try
+            {
+                if (value is Attrib[] attribs && attribs?.Length > 0)
+                {
+                    StringBuilder attribsStringBuider = new StringBuilder();
+                    foreach (Attrib attrib in attribs)
+                    {
+                        if (!string.IsNullOrWhiteSpace(attrib.display))
+                        {
+                            if (attribsStringBuider.Length > 0)
+                            {
+                                attribsStringBuider.Append("\r\n");
+                            }
+                            attribsStringBuider.Append(attrib.display.Replace("{value}", attrib.value));
+                        }
+                    }
+
+                    return attribsStringBuider.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                LogCourier.LogAsync(ex.Message, LogCourier.LogType.Error);
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Dotahold/Converters/ItemAttribsToVisibilityConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dotahold.Data.DataShop;
+using Dotahold.Data.Models;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Dotahold.Converters
+{
+    internal partial class ItemAttribsToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            try
+            {
+                if (value is Attrib[] attribs && attribs?.Length > 0)
+                {
+                    foreach (Attrib attrib in attribs)
+                    {
+                        if (!string.IsNullOrWhiteSpace(attrib.display))
+                        {
+                            return Visibility.Visible;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LogCourier.LogAsync(ex.Message, LogCourier.LogType.Error);
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
