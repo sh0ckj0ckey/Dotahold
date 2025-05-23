ad787735127da58fa0a3030ad30663bc34bfd337
姚一鸣 <yaoyiming123@live.com>
2025-05-23 23:30:41 +0800

    The UniformGridLayout control has been added, under the divine wisdom of Copilot

---
+++ Dotahold/Controls/UniformGridLayout.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.UI.Xaml.Controls;
+using Windows.Foundation;
+
+namespace Dotahold.Controls
+{
+    public partial class UniformGridLayout : NonVirtualizingLayout
+    {
+        private double _minItemWidth = 296.0;
+
+        public double MinItemWidth
+        {
+            get => _minItemWidth;
+            set
+            {
+                if (_minItemWidth != value)
+                {
+                    _minItemWidth = value;
+                    InvalidateMeasure();
+                }
+            }
+        }
+
+        protected override Size MeasureOverride(NonVirtualizingLayoutContext context, Size availableSize)
+        {
+            double width = availableSize.Width;
+            int itemsCount = context.Children.Count;
+
+            if (itemsCount == 0 || width == 0 || double.IsInfinity(width))
+            {
+                return new Size(width, 0);
+            }
+
+            int columns = Math.Max(1, Math.Min(itemsCount, (int)(width / MinItemWidth)));
+            double itemWidth = width / columns;
+
+            double totalHeight = 0;
+            double rowHeight = 0;
+            int columnIndex = 0;
+
+            foreach (var child in context.Children)
+            {
+                child.Measure(new Size(itemWidth, double.PositiveInfinity));
+                rowHeight = Math.Max(rowHeight, child.DesiredSize.Height);
+
+                columnIndex++;
+                if (columnIndex == columns)
+                {
+                    totalHeight += rowHeight;
+                    rowHeight = 0;
+                    columnIndex = 0;
+                }
+            }
+
+            if (columnIndex != 0)
+            {
+                totalHeight += rowHeight;
+            }
+
+            return new Size(width, totalHeight);
+        }
+
+        protected override Size ArrangeOverride(NonVirtualizingLayoutContext context, Size finalSize)
+        {
+            double width = finalSize.Width;
+            int itemsCount = context.Children.Count;
+
+            if (itemsCount == 0 || width == 0)
+            {
+                return finalSize;
+            }
+
+            int columns = Math.Max(1, Math.Min(itemsCount, (int)(width / MinItemWidth)));
+            int rows = (itemsCount + columns - 1) / columns;
+            double itemWidth = width / columns;
+
+            double[] rowHeights = new double[rows];
+
+            for (int i = 0; i < context.Children.Count; i++)
+            {
+                int row = i / columns;
+                rowHeights[row] = Math.Max(rowHeights[row], context.Children[i].DesiredSize.Height);
+            }
+
+            double y = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                double x = 0;
+
+                for (int col = 0; col < columns; col++)
+                {
+                    int index = row * columns + col;
+                    if (index >= itemsCount)
+                    {
+                        break;
+                    }
+
+                    context.Children[index].Arrange(new Rect(x, y, itemWidth, rowHeights[row]));
+                    x += itemWidth;
+                }
+
+                y += rowHeights[row];
+            }
+
+            return finalSize;
+        }
+    }
+}
--- Dotahold/Pages/Heroes/HeroDataPage.xaml.cs
+++ Dotahold/Pages/Heroes/HeroDataPage.xaml.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 using Dotahold.Controls;
 using Dotahold.Data.DataShop;
 using Dotahold.Models;
@@ -30,8 +27,6 @@ namespace Dotahold.Pages.Heroes
 
         private HeroModel? _heroModel;
 
-        private readonly List<HeroFacetCard> _facetCards = [];
-
         public HeroDataPage()
         {
             _viewModel = App.Current.MainViewModel;
@@ -58,7 +53,7 @@ namespace Dotahold.Pages.Heroes
             };
         }
 
-        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
 
@@ -71,130 +66,7 @@ namespace Dotahold.Pages.Heroes
 
                 if (_heroModel is not null)
                 {
-                    await _viewModel.HeroesViewModel.PickHero(_heroModel, _viewModel.AppSettings.LanguageIndex);
-
-                    // 
-                    // TODO: 不能这样做，PickHero会等待图标下载，耗时较久
-                    // 而且此处似乎控件都还没有创建，不应当在这里修改控件
-                    // 只是记录一下思路，以及如下为封装成为一个通用控件
-                    // 
-                    // using Microsoft.UI.Xaml.Controls;
-                    // using Windows.UI.Xaml;
-                    // using Windows.UI.Xaml.Controls;
-                    // using Windows.UI.Xaml.Media;
-                    // using System;
-
-                    // public class AutoWrapGridPanel : Panel
-                    // {
-                    //     public double MinItemWidth
-                    //     {
-                    //         get => (double)GetValue(MinItemWidthProperty);
-                    //         set => SetValue(MinItemWidthProperty, value);
-                    //     }
-                    //     public static readonly DependencyProperty MinItemWidthProperty =
-                    //         DependencyProperty.Register(nameof(MinItemWidth), typeof(double), typeof(AutoWrapGridPanel), new PropertyMetadata(296.0, LayoutPropertyChanged));
-
-                    //     private static void LayoutPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-                    //     {
-                    //         if (d is AutoWrapGridPanel panel)
-                    //             panel.InvalidateMeasure();
-                    //     }
-
-                    //     protected override Size MeasureOverride(Size availableSize)
-                    //     {
-                    //         double width = availableSize.Width;
-                    //         int count = Children.Count;
-                    //         if (count == 0 || double.IsInfinity(width) || width == 0)
-                    //             return new Size(width, 0);
-
-                    //         double minWidth = MinItemWidth;
-                    //         int columns = Math.Max(1, Math.Min(count, (int)(width / minWidth)));
-                    //         double itemWidth = width / columns;
-
-                    //         double x = 0, y = 0, rowHeight = 0;
-                    //         int col = 0;
-                    //         foreach (UIElement child in Children)
-                    //         {
-                    //             child.Measure(new Size(itemWidth, double.PositiveInfinity));
-                    //             rowHeight = Math.Max(rowHeight, child.DesiredSize.Height);
-                    //             col++;
-                    //             if (col == columns)
-                    //             {
-                    //                 y += rowHeight;
-                    //                 rowHeight = 0;
-                    //                 col = 0;
-                    //             }
-                    //         }
-                    //         if (col != 0)
-                    //             y += rowHeight;
-
-                    //         return new Size(width, y);
-                    //     }
-
-                    //     protected override Size ArrangeOverride(Size finalSize)
-                    //     {
-                    //         double width = finalSize.Width;
-                    //         int count = Children.Count;
-                    //         if (count == 0 || width == 0)
-                    //             return finalSize;
-
-                    //         double minWidth = MinItemWidth;
-                    //         int columns = Math.Max(1, Math.Min(count, (int)(width / minWidth)));
-                    //         double itemWidth = width / columns;
-
-                    //         double x = 0, y = 0, rowHeight = 0;
-                    //         int col = 0;
-                    //         foreach (UIElement child in Children)
-                    //         {
-                    //             child.Measure(new Size(itemWidth, double.PositiveInfinity));
-                    //             var childHeight = child.DesiredSize.Height;
-                    //             child.Arrange(new Rect(x, y, itemWidth, childHeight));
-                    //             rowHeight = Math.Max(rowHeight, childHeight);
-                    //             x += itemWidth;
-                    //             col++;
-                    //             if (col == columns)
-                    //             {
-                    //                 x = 0;
-                    //                 y += rowHeight;
-                    //                 rowHeight = 0;
-                    //                 col = 0;
-                    //             }
-                    //         }
-                    //         return finalSize;
-                    //     }
-                    // }
-                    // 
-                    // 
-                    // 使用
-                    //< ItemsControl ItemsSource = "{Binding Facets}" >
-                    //    < ItemsControl.ItemsPanel >
-                    //        < ItemsPanelTemplate >
-                    //            < local:AutoWrapGridPanel MinItemWidth = "296" />
-                    //        </ ItemsPanelTemplate >
-                    //    </ ItemsControl.ItemsPanel >
-                    //    < ItemsControl.ItemTemplate >
-                    //        < DataTemplate >
-                    //            < controls:HeroFacetCard DataContext = "{Binding}" />
-                    //        </ DataTemplate >
-                    //    </ ItemsControl.ItemTemplate >
-                    //</ ItemsControl >
-
-                    _facetCards.Clear();
-                    FacetsGrid.Children.Clear();
-
-                    if (_viewModel.HeroesViewModel.PickedHeroData?.Facets is not null)
-                    {
-                        foreach (var facet in _viewModel.HeroesViewModel.PickedHeroData?.Facets)
-                        {
-                            var facetCard = new HeroFacetCard(facet);
-                            _facetCards.Add(facetCard);
-                            FacetsGrid.Children.Add(facetCard);
-                        }
-                    }
-
-                    DataAttributesScrollViewer.Width = RootGrid.ActualWidth;
-                    FacetsGrid.Width = RootGrid.ActualWidth - 128;
-                    UpdateFacetsLayout();
+                    _ = _viewModel.HeroesViewModel.PickHero(_heroModel, _viewModel.AppSettings.LanguageIndex);
                 }
             }
             catch (Exception ex)
@@ -218,36 +90,6 @@ namespace Dotahold.Pages.Heroes
         {
             DataAttributesScrollViewer.Width = RootGrid.ActualWidth;
             FacetsGrid.Width = RootGrid.ActualWidth - 128;
-            UpdateFacetsLayout();
-        }
-
-        private void UpdateFacetsLayout()
-        {
-            FacetsGrid.RowDefinitions.Clear();
-            FacetsGrid.ColumnDefinitions.Clear();
-
-            double gridWidth = FacetsGrid.ActualWidth > 0 ? FacetsGrid.ActualWidth : FacetsGrid.Width;
-            int minCardWidth = 296;
-            int columnCount = Math.Max(1, Math.Min(_facetCards.Count, (int)(gridWidth / minCardWidth)));
-            int rowCount = (_facetCards.Count + columnCount - 1) / columnCount;
-
-            for (int i = 0; i < columnCount; i++)
-            {
-                FacetsGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-            }
-
-            for (int i = 0; i < rowCount; i++)
-            {
-                FacetsGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
-            }
-
-            for (int i = 0; i < _facetCards.Count; i++)
-            {
-                int row = i / columnCount;
-                int col = i % columnCount;
-                Grid.SetRow(_facetCards[i], row);
-                Grid.SetColumn(_facetCards[i], col);
-            }
         }
 
         /// <summary>
