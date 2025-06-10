dc374497502fd95312cb0bcbd8b96422b999c0b2
姚一鸣 <yaoyiming123@live.com>
2025-06-11 00:29:53 +0800

    Add a default icon for Hero Icon

--- Dotahold/Models/HeroModel.cs
+++ Dotahold/Models/HeroModel.cs
@@ -11,6 +11,11 @@ namespace Dotahold.Models
         /// </summary>
         private static BitmapImage? _defaultHeroImageSource144 = null;
 
+        /// <summary>
+        /// 默认英雄图标
+        /// </summary>
+        private static BitmapImage? _defaultHeroIconSource36 = null;
+
         public DotaHeroModel DotaHeroAttributes { get; private set; }
 
         public AsyncImage HeroImage { get; private set; }
@@ -25,9 +30,15 @@ namespace Dotahold.Models
                 DecodePixelHeight = 144,
             };
 
+            _defaultHeroIconSource36 ??= new BitmapImage(new Uri("ms-appx:///Assets/icon_dota2_color.png"))
+            {
+                DecodePixelType = DecodePixelType.Logical,
+                DecodePixelHeight = 36,
+            };
+
             this.DotaHeroAttributes = hero;
             this.HeroImage = new AsyncImage($"{Dotahold.Data.DataShop.ConstantsCourier.ImageSourceDomain}{this.DotaHeroAttributes.img}", 0, 144, _defaultHeroImageSource144);
-            this.HeroIcon = new AsyncImage($"{Dotahold.Data.DataShop.ConstantsCourier.ImageSourceDomain}{this.DotaHeroAttributes.icon}", 0, 36);
+            this.HeroIcon = new AsyncImage($"{Dotahold.Data.DataShop.ConstantsCourier.ImageSourceDomain}{this.DotaHeroAttributes.icon}", 0, 36, _defaultHeroIconSource36);
         }
     }
 }
