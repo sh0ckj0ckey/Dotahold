57f7461ad28ff49e326c25cfab7143e783bd6b9f
姚一鸣 <yaoyiming123@live.com>
2025-06-17 21:04:59 +0800

    Add back hotkey to HeroesPlayedPage and MatchesPage

--- Dotahold/Pages/Matches/HeroesPlayedPage.xaml.cs
+++ Dotahold/Pages/Matches/HeroesPlayedPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using Dotahold.ViewModels;
+using Windows.System;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -18,13 +20,65 @@ namespace Dotahold.Pages.Matches
             _viewModel = App.Current.MainViewModel;
 
             this.InitializeComponent();
+
+            this.Loaded += (_, _) =>
+            {
+                Window.Current.CoreWindow.Dispatcher.AcceleratorKeyActivated += CoreDispatcher_AcceleratorKeyActivated;
+                Window.Current.CoreWindow.PointerPressed += CoreWindow_PointerPressed;
+                SystemNavigationManager.GetForCurrentView().BackRequested += System_BackRequested;
+            };
+
+            this.Unloaded += (_, _) =>
+            {
+                Window.Current.CoreWindow.Dispatcher.AcceleratorKeyActivated -= CoreDispatcher_AcceleratorKeyActivated;
+                Window.Current.CoreWindow.PointerPressed -= CoreWindow_PointerPressed;
+                SystemNavigationManager.GetForCurrentView().BackRequested -= System_BackRequested;
+            };
         }
 
-        private void BackButton_Click(object sender, RoutedEventArgs e)
+        #region GoBack
+
+        private bool TryGoBack()
         {
             this.Frame.Navigate(typeof(BlankPage));
             this.Frame.ForwardStack.Clear();
             this.Frame.BackStack.Clear();
+            return true;
         }
+
+        private void CoreDispatcher_AcceleratorKeyActivated(CoreDispatcher sender, AcceleratorKeyEventArgs e)
+        {
+            if (e.EventType == CoreAcceleratorKeyEventType.SystemKeyDown
+                && e.VirtualKey == VirtualKey.Left
+                && e.KeyStatus.IsMenuKeyDown == true
+                && !e.Handled)
+            {
+                e.Handled = TryGoBack();
+            }
+        }
+
+        private void System_BackRequested(object? sender, BackRequestedEventArgs e)
+        {
+            if (!e.Handled)
+            {
+                e.Handled = TryGoBack();
+            }
+        }
+
+        private void CoreWindow_PointerPressed(CoreWindow sender, PointerEventArgs e)
+        {
+            if (e.CurrentPoint.Properties.IsXButton1Pressed)
+            {
+                e.Handled = TryGoBack();
+            }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            TryGoBack();
+        }
+
+        #endregion
+
     }
 }
--- Dotahold/Pages/Matches/MatchesPage.xaml.cs
+++ Dotahold/Pages/Matches/MatchesPage.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using Dotahold.Data.DataShop;
 using Dotahold.ViewModels;
+using Windows.System;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
@@ -20,13 +23,29 @@ namespace Dotahold.Pages.Matches
             _viewModel = App.Current.MainViewModel;
 
             this.InitializeComponent();
+
+            this.Loaded += (_, _) =>
+            {
+                Window.Current.CoreWindow.Dispatcher.AcceleratorKeyActivated += CoreDispatcher_AcceleratorKeyActivated;
+                Window.Current.CoreWindow.PointerPressed += CoreWindow_PointerPressed;
+                SystemNavigationManager.GetForCurrentView().BackRequested += System_BackRequested;
+            };
+
+            this.Unloaded += (_, _) =>
+            {
+                Window.Current.CoreWindow.Dispatcher.AcceleratorKeyActivated -= CoreDispatcher_AcceleratorKeyActivated;
+                Window.Current.CoreWindow.PointerPressed -= CoreWindow_PointerPressed;
+                SystemNavigationManager.GetForCurrentView().BackRequested -= System_BackRequested;
+            };
         }
 
-        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
+            base.OnNavigatedTo(e);
+
             try
             {
-                if (!string.IsNullOrWhiteSpace(_viewModel.AppSettings.SteamID))
+                if (e.NavigationMode == NavigationMode.New && !string.IsNullOrWhiteSpace(_viewModel.AppSettings.SteamID))
                 {
                     await _viewModel.MatchesViewModel.LoadPlayerAllMatches(_viewModel.AppSettings.SteamID);
                     _viewModel.MatchesViewModel.LoadMoreMatches();
@@ -64,11 +83,49 @@ namespace Dotahold.Pages.Matches
             }
         }
 
-        private void BackButton_Click(object sender, RoutedEventArgs e)
+        #region GoBack
+
+        private bool TryGoBack()
         {
             this.Frame.Navigate(typeof(BlankPage));
             this.Frame.ForwardStack.Clear();
             this.Frame.BackStack.Clear();
+            return true;
         }
+
+        private void CoreDispatcher_AcceleratorKeyActivated(CoreDispatcher sender, AcceleratorKeyEventArgs e)
+        {
+            if (e.EventType == CoreAcceleratorKeyEventType.SystemKeyDown
+                && e.VirtualKey == VirtualKey.Left
+                && e.KeyStatus.IsMenuKeyDown == true
+                && !e.Handled)
+            {
+                e.Handled = TryGoBack();
+            }
+        }
+
+        private void System_BackRequested(object? sender, BackRequestedEventArgs e)
+        {
+            if (!e.Handled)
+            {
+                e.Handled = TryGoBack();
+            }
+        }
+
+        private void CoreWindow_PointerPressed(CoreWindow sender, PointerEventArgs e)
+        {
+            if (e.CurrentPoint.Properties.IsXButton1Pressed)
+            {
+                e.Handled = TryGoBack();
+            }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            TryGoBack();
+        }
+
+        #endregion
+
     }
 }
